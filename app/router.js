1a3ad143bb3ea187f7b33932423b8c098da1ade8
codeliling <maximuslee@126.com>
2019-04-19 10:00:21 +0800

    增加功能

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+
+  router.get('/website/sms/createSmsMessage', controller.smsMessage.createSmsMessage);
+  router.get('/website/sms/vertifySms', controller.smsMessage.vertifySms);
+
+  router.resources('/website/user',controller.website.user);
 };
