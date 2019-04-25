40c9cdd194a6971672ea263870064da80cd7db97
codeliling <maximuslee@126.com>
2019-04-25 11:35:00 +0800

    fix bugs

--- app/service/news.js
+++ app/service/news.js
@@ -5,7 +5,7 @@ const Service = require('egg').Service;
 class NewsService extends Service {
 
   async listNews({ offset = 0, limit = 10, language = 0 }){
-    let resultObj = await this.ctx.mode.News.listNews({offset,limit,language});
+    let resultObj = await this.ctx.model.News.listNews({offset,limit,language});
     const helper = this.ctx.helper;
     resultObj.rows.forEach((element, index)=>{
       element.thumb = helper.signatureUrl(helper.newsPath + element.thumb, "thumb_300_300");
@@ -14,24 +14,24 @@ class NewsService extends Service {
   }
 
   async getTopNews({ limit = 10, language = 0 }){
-    return await this.ctx.mode.News.getTopNews({limit,language});
+    return await this.ctx.model.News.getTopNews({limit,language});
   }
 
   async createNews(news){
-    return await this.ctx.mode.News.createNews(news);
+    return await this.ctx.model.News.createNews(news);
   }
 
   async delNewsById(id){
-    return await this.ctx.mode.News.delNewsById(id);
+    return await this.ctx.model.News.delNewsById(id);
   }
 
   async updateNews({id,updates}){
-    return await this.ctx.mode.News.updateNews({id,updates});
+    return await this.ctx.model.News.updateNews({id,updates});
   }
 
   async getNewsById(id){
     const helper = this.ctx.helper;
-    let resultObj = await this.ctx.mode.News.getNewsById(id);
+    let resultObj = await this.ctx.model.News.getNewsById(id);
     resultObj.thumb = helper.signatureUrl(helper.newsPath + resultObj.thumb);
     return resultObj;
   }
