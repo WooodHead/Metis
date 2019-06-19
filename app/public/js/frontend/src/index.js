84de935fb5b440f06b00bb8d3b2c6daf56061dda
gansiqiu <1223720954@qq.com>
2019-06-19 15:47:24 +0800

    fix bugs

--- app/public/js/frontend/src/index.js
+++ app/public/js/frontend/src/index.js
@@ -1,22 +1,3 @@
-$(document).ready(function(){
-
-	var ua = navigator.userAgent;
-
-	var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
-
-	isIphone =!ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
-
-	isAndroid = ua.match(/(Android)\s+([\d.]+)/),
-
-	isMobile = isIphone || isAndroid;
-
-	if(isMobile){
-		window.location.href = "/DesignYL/mobile/index";
-	}else{
-
-	}
-})
-
 'use strict';
 
 var appServer = 'http://localhost:8080/dcpro/sigUploadKey/1';
